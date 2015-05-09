77bb83b1d06ed9c3648e1adf4eb7d944904cde57
green <green@cganalytics.org>
2015-05-08 19:07:16 -0700

    hamming distance exercise

---
+++ hamming/hamming.cpp
@@ -0,0 +1,21 @@
+#include "hamming.h"
+
+#include <string>
+#include <stdexcept>
+
+namespace hamming
+{
+  int compute(const std::string& strand1, const std::string& strand2)
+  {
+    int count = 0;
+    if (strand1.length() != strand2.length()) {
+      throw std::domain_error("strand lengths not equal");
+    }
+    for (int i=0; i<strand1.length(); i++) {
+      if (strand1[i] != strand2[i]) {
+	count++;
+      }
+    }
+    return count;
+  }
+}
