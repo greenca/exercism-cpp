a5de77a57a880f6ca79304ffce97dd13ea9c7add
green <green@cganalytics.org>
2015-05-22 14:59:34 -0700

    formatting issues

--- hamming/hamming.cpp
+++ hamming/hamming.cpp
@@ -5,17 +5,17 @@
 
 namespace hamming
 {
-  size_t compute(const std::string& strand1, const std::string& strand2)
-  {
+size_t compute(const std::string& strand1, const std::string& strand2)
+{
     int count = 0;
     if (strand1.length() != strand2.length()) {
-      throw std::domain_error("strand lengths not equal");
+        throw std::domain_error("strand lengths not equal");
     }
     for (size_t i=0; i<strand1.length(); i++) {
-      if (strand1[i] != strand2[i]) {
-	count++;
-      }
+        if (strand1[i] != strand2[i]) {
+            count++;
+        }
     }
     return count;
-  }
+}
 }
