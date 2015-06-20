2d46557fbd8c48276317b945dd6f407c560ff253
green <green@cganalytics.org>
2015-06-19 22:58:05 -0700

    formating

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -6,12 +6,12 @@ namespace binary
 unsigned int convert(const std::string& bin) {
     unsigned int result = 0;
     for (std::string::const_iterator it=bin.cbegin(); it!=bin.cend(); ++it) {
-      result *= 2;
-      if (*it == '1') {
-	result++;
-      } else if (*it != '0') {
-	return 0;
-      }
+        result *= 2;
+        if (*it == '1') {
+            result++;
+        } else if (*it != '0') {
+            return 0;
+        }
     }
     return result;
 }
