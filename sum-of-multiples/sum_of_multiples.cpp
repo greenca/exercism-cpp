728de49ca7431e11f7d870f079207db258f44434
green <green@cganalytics.org>
2015-06-19 22:56:07 -0700

    updated sum of multiples exercise

--- sum-of-multiples/sum_of_multiples.cpp
+++ sum-of-multiples/sum_of_multiples.cpp
@@ -6,12 +6,12 @@ int to(int n) {
     return to({3, 5}, n);
 }
 
-int to(std::vector<int> mults, int n) {
+int to(std::set<int> mults, int n) {
     int result = 0;
-    for (int i = 0; i < n; i++) {
-        for (std::vector<int>::iterator it = mults.begin(); it != mults.end(); ++it) {
+    for (int i = 1; i < n; i++) {
+        for (std::set<int>::iterator it = mults.begin(); it != mults.end(); ++it) {
             if (i % *it == 0) {
-		result += i;
+                result += i;
                 break;
             }
         }
--- sum-of-multiples/sum_of_multiples.h
+++ sum-of-multiples/sum_of_multiples.h
@@ -1,12 +1,12 @@
 #ifndef SUM_OF_MULTIPLES_H
 #define SUM_OF_MULTIPLES_H
 
-#include <vector>
+#include <set>
 
 namespace sum_of_multiples
 {
 int to(int n);
-int to(std::vector<int> mults, int n);
+int to(std::set<int> mults, int n);
 }
 
 #endif
